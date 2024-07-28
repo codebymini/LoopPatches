f50d5fd8054c6f85c425e2e1b8ce8fb06c3a99d7
Daniel Johansson <daniel@codebymini.se>
2024-07-28 23:26:26 +0200

    Init

---
+++ menu.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+function menu() {
+    local name="$1"
+    local patch_file="$2"
+    local directory="$3"
+
+    # Check if the patch has already been applied
+    if git apply --reverse --check "${mytmpdir}/${patch_file}.patch" --directory="${directory}" >/dev/null 2>&1; then
+        echo "${name} *** Patch is applied ***"
+    else
+        # Try to apply the patch
+        if git apply --check "${mytmpdir}/${patch_file}.patch" --directory="${directory}" >/dev/null 2>&1; then
+            echo "${name}" # - Patch can be applied
+        else
+            echo "${name} !!! Patch can't be applied !!!"
+        fi
+    fi
+}
+
+function apply() {
+    local name="$1"
+    local patch_file="${mytmpdir}/$2.patch"
+    local directory="$3"
+    echo 
+    # Check if the patch has already been applied
+    if git apply --reverse --check "${patch_file}" --directory="${directory}" >/dev/null 2>&1; then
+        echo "${name} *** Patch is applied ***"
+    else
+        # Try to apply the patch
+        if git apply --check "${patch_file}" --directory="${directory}" >/dev/null 2>&1; then
+            echo "${name} - Applying..."
+            git apply "${patch_file}" --directory="${directory}"
+            exit_code=$?
+            if [ $exit_code -eq 0 ]; then
+                echo "Successful!"
+            else
+                echo "Failed!"
+            fi
+        else
+            echo "${name} !!! Patch can't be applied !!!"
+        fi
+    fi
+}
+
+function revertmenu() {
+    local name="$1"
+    local patch_file="$2"
+    local directory="$3"
+
+    # Check if the patch has already been applied
+    if git apply --reverse --check "${mytmpdir}/${patch_file}.patch" --directory="${directory}" >/dev/null 2>&1; then
+        echo "${name}" # - Patch can be reverted
+    else
+        # Try to apply the patch
+        if git apply --check "${mytmpdir}/${patch_file}.patch" --directory="${directory}" >/dev/null 2>&1; then
+            echo "${name} *** Patch is not applied ***"
+        else
+            echo "${name} !!! Reverting or applying the patch is not possible !!!"
+        fi
+    fi
+}
+
+function revert() {
+    local name="$1"
+    local patch_file="${mytmpdir}/$2.patch"
+    local directory="$3"
+    echo 
+    if git apply --reverse --check "${patch_file}" --directory="${directory}" >/dev/null 2>&1; then
+        echo "${name} - Reversing..."
+        git apply --reverse "${patch_file}" --directory="${directory}"
+        exit_code=$?
+        if [ $exit_code -eq 0 ]; then
+            echo "Successful!"
+        else
+            echo "Failed!"
+        fi
+    else
+        if git apply --check "${mytmpdir}/${patch_file}.patch" --directory="${directory}" >/dev/null 2>&1; then
+            echo "${name} *** Patch is not applied ***"
+        else
+            echo "${name} !!! Reverting or applying the patch is not possible !!!"
+        fi
+    fi
+}
+
+function add_patch() {
+    name+=("$1")
+    file+=("$2")
+    folder+=("$3")
+}
+
+# Deletes the temp directory
+function cleanup {      
+  echo "Deleting temp working directory $mytmpdir"
+  rm -r "$mytmpdir"
+  tput cuu1 && tput el
+}
+
+if [ "$(basename "$PWD")" != "LoopWorkspace" ]; then
+    target_dir="$(find /Users/$USER/Downloads/BuildLoop -maxdepth 2 -type d -name LoopWorkspace -exec dirname {} \; -exec stat -f "%B %N" {} \; | sort -rn | awk '{print $2}' | head -n 1)"
+    if [ -z "$target_dir" ]; then
+        echo "Error: No folder containing LoopWorkspace found."
+    else
+        echo "Navigating to $target_dir"
+        cd "$target_dir"
+    fi
+fi
+
+echo "Loop patch selection"
+
+# Verify current folder
+if [ $(basename $PWD) = "LoopWorkspace" ]; then
+    echo "Creating temporary folder"
+    workingdir=$PWD
+    mytmpdir=$(mktemp -d)
+
+    # Check if tmp dir was created
+    if [[ ! "$mytmpdir" || ! -d "$mytmpdir" ]]; then
+        echo "Could not create temporary folder"
+        exit 1
+    fi
+    tput cuu1 && tput el
+
+    # Register the cleanup function to be called on the EXIT signal
+    trap cleanup EXIT
+
+    #DEBUG
+    #echo $mytmpdir
+
+    #Declare all patches
+    #Using simple arrays since bash 3 which is shipped with mac does not support associative arrays
+    name=()
+    file=()
+    folder=()
+
+
+    add_patch "Dash Fast Forward" "fast_forward" ""
+    add_patch "Basic I:C Bolus Calculation" "basicICBolusCalc" ""
+
+    echo "Downloading patches, please wait..."
+    cd $mytmpdir
+    for i in ${!name[@]}
+    do
+        curl -fsSLOJ "https://raw.githubusercontent.com/codebymini/LoopPatches/master/${file[$i]}.patch"
+    done
+    tput cuu1 && tput el
+
+    cd $workingdir
+
+    while true; do
+        echo
+        echo "Select a patch you want to apply:"
+        echo "a) Apply all patches"
+        for i in ${!name[@]}
+        do
+            menu "${i}) ${name[$i]}" "${file[$i]}" "${folder[$i]}";
+        done        
+
+        echo "r) Revert a patch"
+        echo "q) Quit"
+
+        read -p "Enter your choice: " choice
+        if [[ $choice =~ ^[0-9]+$ && $choice -ge 0 && $choice -lt ${#name[@]} ]]; then
+            apply "${name[$choice]}" "${file[$choice]}" "${folder[$choice]}";
+        elif [[ $choice == "r" ]]; then
+            echo 
+            echo "Select a patch to revert:"
+            for i in ${!name[@]}
+            do
+                revertmenu "${i}) ${name[$i]}" "${file[$i]}" "${folder[$i]}";
+            done        
+            echo "*) Any other key will exit to last menu"
+            read -p "Enter your choice: " choice
+            if [[ $choice =~ ^[0-9]+$ && $choice -ge 0 && $choice -lt ${#name[@]} ]]; then
+                revert "${name[$choice]}" "${file[$choice]}" "${folder[$choice]}";
+            fi
+        elif [[ $choice == "a" ]]; then
+            for i in ${!name[@]}
+            do
+                apply "${name[$i]}" "${file[$i]}" "${folder[$i]}";
+            done        
+        elif [[ $choice == "q" ]]; then
+            exit 0
+        else
+            echo
+            echo "Invalid choice. Try again."
+        fi
+    done
+else
+    exit 1
+fi
---
+++ readme.md
@@ -0,0 +1,74 @@
+# Loop patches
+
+These patches are intended for Loop 3.0 or 3.2, with or without loop n learn patches.  
+If you are missing a patch here, head over to LnL since some has been moved there.
+
+## How to apply a patch when building with Xcode:
+
+First, download Loop by using the loop and learn build script described here: https://www.loopandlearn.org/build-select/
+
+### To select apply (or revert) patches, run the following command:
+
+```console
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/codebymini/LoopPatches/master/menu.sh)"
+```
+
+Copy the command using the copy-button (as shown in the picture below), paste (⌘ V) it into your terminal, and press Enter.
+
+![Loop](img/copy_command.png)
+
+## How to apply a patch when building with github:
+
+Use this page to select the patches you want:  
+https://htmlpreview.github.io/?https://github.com/codebymini/LoopPatches/blob/master/loop_patches.html  
+Copy the code using the copy-button
+In github, edit ".github/workflows/build_loop.yml" and paste the code there.  
+Make sure "- name: Customize Loop..." is not already in the file, in that case remove the duplicate.
+
+&nbsp;
+
+## Dash Fast Forward
+
+When pairing a new pod, the abundance of unnecessary clicks and confirmations in Loop 3.x can be quite cumbersome. As an experienced looper, you likely breeze through them as quickly as possible without actually reading them. This patch streamlines the process by eliminating the unnecessary dialogs and retaining only the essential ones—"Pair Pod" and "Insert Cannula"—where actual actions take place. As a result, the patch effectively reduces clicks and enhances the user experience.
+&nbsp;
+
+## Basic I:C Bolus Calculation
+
+**Please note that this patch is still in testing and may receive changes.**
+
+The patch introduces a new feature in the meal bolus screen that presents a basic calculation for an insulin-to-carb (I:C) bolus that does not factor in the glucose prediction. This feature provides users with decision support for manually adjusting their bolus based on their personal experience. An example of when this feature could be useful is if a user has recently treated a low or trending low without entering the treatment into Loop. In such a scenario, Loop may underestimate the amount of insulin needed for a meal and even recommend against bolusing - Loop would then wait with the insulin until blood glucose rises, possibly resulting in a spike.
+
+The Basic I:C Bolus Calc row displays the calculated bolus based on the user's insulin-to-carb ratio, the entered carbohydrates, and existing carbs on board. It ignores current bg value, glucose predictions, and override, but considers positive insulin on board (IOB) and ignores negative IOB to avoid over-bolusing. The suggested bolus value is displayed in red, indicating that it is calculated differently from Loop's default recommendation.
+
+> **Warning: This feature is designed as a basic calculation tool and does not consider all factors that may affect blood sugar levels. The calculated bolus provided by the "Basic I:C Bolus Calc" is _not_ a recommended bolus and the feature may not be suitable for all users. Giving too much insulin can be dangerous and cause low blood sugar levels. Always consult with your healthcare provider before making any changes to your diabetes management plan.**
+
+To use this feature, tap the "Basic I:C Bolus Calc" row, and the app will update the bolus entry field with the calculated value. Users can then if desired adjust the value and deliver the bolus. Loop's normal calculation and recommendation remains the default. There are scenarios where Loop would recommend more insulin than the basic calculation, such as when the blood glucose value is too high and Loop includes a correction in the dosage.
+
+Calculation Example:  
+Assume a user has an insulin-to-carb ratio of 1:10. They enter a meal with 50 grams of carbohydrates, their current positive IOB is 1 unit, and they have 20 grams of carbs on board. Using the Basic I:C Bolus Calc, the calculation would be as follows:  
+Total carbs: 50 grams of entered carbs + 20 grams of carbs on board = 70 grams  
+Unadjusted bolus: 70 grams of total carbs / 10 (I:C ratio) = 7 units of insulin  
+Bolus with IOB: 7 units (unadjusted) - 1 unit (positive IOB) = 6 units  
+The Basic I:C Bolus Calc would show a 6-unit bolus value.
+
+Additionally, this patch restores the feature to click on Loop's recommended value to copy it to the entered bolus value. This feature was removed in Loop 3.
+&nbsp;
+
+## How to apply a patch:
+
+First, download Loop Follow dev by using the loop and learn build script described here: https://www.loopandlearn.org/build-select/
+
+### Run the command below to select the patches you want to apply or revert
+
+```console
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/codebymini/LoopPatches/master/lf.sh)"
+```
+
+Please use the copy-button (as shown in the picture below) to copy the command, then paste it (⌘ V) into your terminal and press Enter.
+
+![Loop](img/copy_command.png)
+
+## PreMeal
+
+The graph displays the PreMeal period as an orange band located below the green override band.
+&nbsp;
